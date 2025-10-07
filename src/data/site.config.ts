c5812f43fab0e8337edd93d71ddc2d59e493f7bf
Mochammad Farros Fatchur Roji <farros.alfatih@gmail.com>
2025-10-07 11:07:57 +0700

    up

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,12 +10,13 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
-	paginationSize: 6 // Number of posts per page
+	site: 'https://blog.sonushub.id/',
+	author: 'SonusHUB',
+	title: 'Blog SonusHUB', // Judul lebih deskriptif untuk SEO
+	description:
+		'Wawasan terbaru seputar material kelistrikan, efisiensi energi, dan teknologi energi terbarukan.', // Deskripsi sesuai konteks bisnis Anda
+	lang: 'id-ID', // Ganti ke Bahasa Indonesia
+	ogLocale: 'id_ID', // Ganti ke locale Indonesia
+	shareMessage: 'Bagikan artikel ini', // Pesan saat berbagi di media sosial
+	paginationSize: 6
 }
